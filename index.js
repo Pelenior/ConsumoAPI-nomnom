73b15d1963e002177c03157ef8f0ecb8d76b2cf0
enior <psantosdemora@gmail.com>
2025-05-30 13:50:16 +0200

    me cago en el puto auto stash before JODEEEER

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-window.addEventListener('DOMContentLoaded', function()
-{
+window.addEventListener('DOMContentLoaded', function () {
     //get the elements using the DOM
     let btnAsk = document.getElementById("btnAsk");
     let btnNext = this.document.getElementById("btnNext");
@@ -15,18 +14,14 @@ window.addEventListener('DOMContentLoaded', function()
     btnAsk.addEventListener('click', askCard);
     btnPrev.addEventListener('click', prevCard);
     btnNext.addEventListener('click', nextCard);
-    cardInput.addEventListener("keyup", ({key}) =>
-    {
-        if (key === "Enter")
-            {
-                askCard();
-            }
+    cardInput.addEventListener("keyup", ({ key }) => {
+        if (key === "Enter") {
+            askCard();
+        }
     })
 
-    function askCard()
-    {
-        if(cardName == cardInput.value)
-        {
+    function askCard() {
+        if (cardName == cardInput.value) {
             console.log("Same search, not consuming API");
             return;
         }
@@ -34,45 +29,37 @@ window.addEventListener('DOMContentLoaded', function()
         cardId = 0;
 
         fetch("https://api.pokemontcg.io/v2/cards?q=name:" + cardName,
-        {
-            headers: { "X-Api-Key": "39937099-87b7-425f-b19d-bad8442bc588" }
-        }).then(function(res)
-        {
-            console.log(res.status);
-            if(!res.ok)
             {
-                throw new Error("Pokémon not found");
-            }
+                headers: { "X-Api-Key": "39937099-87b7-425f-b19d-bad8442bc588" }
+            }).then(function (res) {
+                console.log(res.status);
+                if (!res.ok) {
+                    throw new Error("Pokémon not found");
+                }
 
-            return res.json();
-        }).then(function(data)
-        {
-            globalData = data;
-            console.log(globalData);
-            showData(globalData);
-        }).catch(function(error)
-        {
-            console.log(error);
-        })
+                return res.json();
+            }).then(function (data) {
+                globalData = data;
+                console.log(globalData);
+                showData(globalData);
+            }).catch(function (error) {
+                console.log(error);
+            })
     }
 
-    function showData(data)
-    {
-        if(cardId == -1)
-        {
+    function showData(data) {
+        if (cardId == -1) {
             console.log("id -1 not valid, trying to go to id: " + (data.data.length - 1));
             cardId = data.data.length - 1;
         }
         let card = data.data[cardId];
 
         //first error catch (in case you went over the limit by going to next or prev card)
-        if (!card)
-        {
+        if (!card) {
             cardId = 0;
             card = data.data[cardId];
             //second error catch (wrong input name, pokemon doesn't exist)
-            if(!card)
-            {
+            if (!card) {
                 divInfo.innerHTML = "<p>No card found.</p>";
                 cardImg.src = "";
                 return;
@@ -81,27 +68,45 @@ window.addEventListener('DOMContentLoaded', function()
 
         let urlImg = card.images.large;
         let name = card.name.toUpperCase();
+        let artist = card.artist;
+
+        let cardmarket = "";
+        if (card.cardmarket.url !== undefined) {
+            cardmarket = card.cardmarket.url;
+        }
+        let legalities = card.legalities.unlimited;
 
         cardImg.src = urlImg;
 
         let setName = card.set.name;
         let setImage = card.set.images.logo;
 
-        if(card.supertype == "Pokémon")
-        {
+        let prices = card.tcgplayer.prices;
+        let priceHTML = '';
+
+        Object.entries(prices).forEach(([type, priceData]) => {
+            if (priceData.mid !== undefined) {
+                priceHTML += `<p><strong>${type} Mid Price:</strong> $${priceData.mid}</p>`;
+            }
+        });
+
+        if (card.supertype == "Pokémon") {
             let hp = card.hp;
             let mainType = card.types[0];
 
             divInfo.innerHTML = `
+                <p><strong>Legality: ${legalities}</strong></p>
                 <p><strong>Name: ${name}</strong></p>
                 <p><strong>HP: ${hp}</strong></p>
                 <p><strong>Main Type: ${mainType}</strong></p>
+                <p><strong><strong>Artist: </strong>${artist}</p>
+                ${priceHTML}
+                <a href="${cardmarket}" class="cardmarket" style="text-decoration: none;"><strong style="color: black;">CardMaket: </strong> Link</a>
                 <p><strong>Set: ${setName}</strong></p>
                 <img src="${setImage}" width="80%"></img>
             `;
         }
-        else if(card.supertype == "Trainer")
-        {
+        else {
             divInfo.innerHTML = `
                 <p><strong>Name: ${name}</strong></p>
                 <p><strong>Set: ${setName}</strong></p>
@@ -109,15 +114,13 @@ window.addEventListener('DOMContentLoaded', function()
             `;
         }
 
-        
+
     }
-    function prevCard()
-    {
+    function prevCard() {
         cardId -= 1;
         showData(globalData);
     }
-    function nextCard()
-    {
+    function nextCard() {
         cardId += 1;
         showData(globalData);
     }
